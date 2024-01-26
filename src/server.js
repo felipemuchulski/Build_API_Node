ef1d161635c34d5d0afba425fe550486e5f7678c
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2024-01-26 10:04:01 -0300

    Add route.params and query.params

--- src/server.js
+++ src/server.js
@@ -4,6 +4,24 @@ const express = require("express");
 //running express on code 
 const app = express();
 
+//route.params
+app.get('/message/:id/:user', (request, response) => {
+    const {id, user} = request.params;
+
+    response.send(`
+    ID Message: ${id}
+    For user: ${user}`);
+} )
+
+// query.params
+app.get('/user', (request, response) => {
+    const {page, limit} = request.query;
+
+    response.send(`
+    page: ${page}. limit ${limit}`);
+
+});
+
 //is host port
 const PORT = 3333;
 //Checks if the port is running
