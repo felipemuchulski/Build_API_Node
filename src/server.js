918bf6e3eae8eb1fcbc08aa7aab451317b46d3c5
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2024-01-29 11:12:19 -0300

    use method post and get

--- src/server.js
+++ src/server.js
@@ -3,24 +3,36 @@ const express = require("express");
 
 //running express on code 
 const app = express();
+app.use(express.json());
 
 //route.params
-app.get('/message/:id/:user', (request, response) => {
-    const {id, user} = request.params;
+// app.get('/message/:id/:user', (request, response) => {
+//     const {id, user} = request.params;
 
-    response.send(`
-    ID Message: ${id}
-    For user: ${user}`);
-} )
+//     response.send(`
+//     ID Message: ${id}
+//     For user: ${user}`);
+// } )
 
 // query.params
-app.get('/user', (request, response) => {
-    const {page, limit} = request.query;
+// app.get('/user', (request, response) => {
+//     const {page, limit} = request.query;
 
-    response.send(`
-    page: ${page}. limit ${limit}`);
+//     response.send(`
+//     page: ${page}. limit ${limit}`);
 
-});
+// });
+
+//method post 
+app.post('/users', (request, response) => {
+    const { name, email, password } = request.body;
+
+    response.json({name, email, password})
+
+    //or
+    // response.send(`Username: ${name} e-mail: ${email}, `)
+    // response.send("You call post")
+})
 
 //is host port
 const PORT = 3333;
