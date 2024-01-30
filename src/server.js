8d806686c67d108da609d4a90eff55d50247f0e8
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2024-01-30 12:52:43 -0300

    Configured express-async-error for client error or internal server error

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
+require("express-async-error")
 //express declared
 const express = require("express");
 
+const AppError = require("./utils/AppError")
+
+
+//import routes
 const routes = require("./routes")
 
 //running express on code 
@@ -8,6 +13,21 @@ const app = express();
 app.use(express.json());
 
 app.use(routes)
+
+
+app.use((error, request, response, next) => {
+    if(error instanceof AppError) {
+        return response.status(error.statusCode).json({
+            status: "error",
+            message: error.message
+        })
+    }
+
+    return response.status(500).json({
+        stauts: "error",
+        message: 'Internal server error'
+    })
+})
 
 
 const PORT = 3333;
