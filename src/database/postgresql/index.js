7b50a56b20637b10dc1d84a15f3a4e95f507a9cd
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2024-02-13 20:26:57 -0300

    Configurado conexão com banco de dados postgresql

--- src/database/postgresql/index.js
+++ src/database/postgresql/index.js
@@ -6,25 +6,12 @@ const client = new Client({
     port: 5432,
     database: "Node_API"
 });
-const path = require("path")
 
 
 async function pgConnection(){
-    const database = await client.connect({
-        filename: path.resolve(__dirname, "..", "database.db"),
-
-    })
-    return database
+   await client.connect();
 }
 
 
-// async function sqliteConnection(){
-//     const database = await sqlite.open({
-//         filename: path.resolve(__dirname, "..", "database.db"),
-//         driver: sqlite3.Database
-//     })
-
-//     return database;
-// }
 
 module.exports = pgConnection;
