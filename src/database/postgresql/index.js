dac488d8ec40046cf06f29dfdf516b820d45c7df
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2024-02-15 23:29:11 -0300

    Alterada configuração para executar a função

--- src/database/postgresql/index.js
+++ src/database/postgresql/index.js
@@ -1,24 +1,14 @@
-const Client = require('pg').Client
-const client = new Client({
-    user: "postgres",
-    password: "91584072",
-    host: "localhost",
-    port: 5432,
-    database: "Node_API"
-});
-
+const connectionString = require("../postgresql/connect_database");
 
 async function pgConnection() {
-    try {
-        await client.connect();
-        console.log('Conexão com o PostgreSQL estabelecida com sucesso!');
-        return client;
-    } catch (error) {
-        console.error('Erro ao conectar com o PostgreSQL:', error);
-        throw error;
-    }
+  try {
+    await connectionString.connect();
+    console.log("Conexão com o PostgreSQL estabelecida com sucesso!");
+    return connectionString;
+  } catch (error) {
+    console.error("Erro ao conectar com o PostgreSQL:", error);
+    throw error;
+  }
 }
-
-
 
 module.exports = pgConnection;
