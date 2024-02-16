dfff6331eb4d58aa41b3d1539e9cbec08e2d7e8e
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2024-02-15 23:29:48 -0300

    Atualizada maneira de conexão com o banco para efetuar criação

--- src/database/postgresql/migrations/index.js
+++ src/database/postgresql/migrations/index.js
@@ -1,17 +1,19 @@
 const createUsers = require("../migrations/createUsers")
 const pgConnection = require('../index')
 
+ const connect = pgConnection();
+
 async function migrationsRun() {
     const schemas = [
         createUsers
     ].join('');
 
     try {
-        const db = await pgConnection();
+        const db = await connect;
         await db.query(schemas);
         console.log('Migrations executadas com sucesso.');
     } catch (error) {
-        console.error(error);
+        console.log('Já existe essa tabela');
     }
 }
 
