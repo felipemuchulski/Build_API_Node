db01bb891300a64bfc3d760fd74c87f596d1636f
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2024-02-15 23:14:20 -0300

    Efetuada verificação de usuário com o banco

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,17 +1,26 @@
-const AppError = require("../utils/AppError")
+const AppError = require("../utils/AppError");
+const connectionString = require('../database/postgresql/connect_database');
 
 class UserController {
- create(request, response) {
-    const {name, email, password} = request.body;
+  async create(request, response) {
+    const { name, email, password } = request.body;
 
-    if(!name) {
-        throw new AppError("user not defined")
+    try {
+      const checkUserExists = await connectionString.query("SELECT * FROM users WHERE email = $1", [email]);
+
+      if (checkUserExists.length > 0) {
+        throw new AppError('E-mail já cadastrado', 400);
+      }
+
+      // Lógica para criar o usuário no banco de dados
+
+      response.status(201).json('to aqui');
+    } catch (error) {
+      console.error(error);
+      response.status(error.statusCode || 500).json({ error: error.message });
     }
-
-
-    response.status(201).json({name, email, password});
- }
-    
+  }
 }
 
 module.exports = UserController;
+
