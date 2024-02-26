338f1baf9abecb6e3cf82e65b6567e89ac305d5e
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2024-02-26 13:07:48 -0300

    Criada uma migration nova

--- src/database/knex/migrations/20240223155620_createNotes.js
+++ src/database/knex/migrations/20240223155620_createNotes.js
@@ -1,15 +1,23 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = knex.schema.createTalbe("note", table => {
-
-}) ;
+const knex = require("knex");
+const { table } = require("..");
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+exports.up = (knex) => knex.schema.createTable("note", (table) => {
+    table.increments("id");
+    table.text("title");
+    table.text("description");
+    table.integer("user_id").references("id_users").inTable("users")
+
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("update_at").defaultTo(knex.fn.now());
+
+});
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = (knex) => knex.schema.dropTable("notes");
