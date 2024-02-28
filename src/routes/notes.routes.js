31839ae7ca0b53ba1cb687c8e2684dca04f5d094
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2024-02-27 21:22:28 -0300

    Efetuada configuração de novos modulos nas rotas para consultas

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -5,7 +5,9 @@ const NotesController = require('../controllers/NoteController');
 const notesRoutes = Router();
 const notesController = new NotesController();
 
-
+notesRoutes.get('/', notesController.index);
 notesRoutes.post('/:user_id', notesController.create);
+notesRoutes.get('/:note_id', notesController.show);
+notesRoutes.delete('/:note_id', notesController.delete)
 
 module.exports = notesRoutes;
