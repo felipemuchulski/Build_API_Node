caa5dd5e8330ce81ceb8c22b2f138ea0a81fc6e7
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2024-03-06 19:18:26 -0300

    Identado

--- src/server.js
+++ src/server.js
@@ -1,40 +1,34 @@
-require("express-async-error")
+require("express-async-error");
 
 const migrationsRun = require("./database/postgresql/migrations/index");
 //express declared
 const express = require("express");
 
-const AppError = require("./utils/AppError")
-
+const AppError = require("./utils/AppError");
 
 //import routes
-const routes = require("./routes")
+const routes = require("./routes");
 migrationsRun();
 
-
-//running express on code 
+//running express on code
 const app = express();
 app.use(express.json());
 
-app.use(routes)
-
-
-
+app.use(routes);
 
 app.use((error, request, response, next) => {
-    if(error instanceof AppError) {
-        return response.status(error.statusCode).json({
-            status: "error",
-            message: error.message
-        })
-    }
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
+    });
+  }
 
-    return response.status(500).json({
-        stauts: "error",
-        message: 'Internal server error'
-    })
-})
-
+  return response.status(500).json({
+    stauts: "error",
+    message: "Internal server error",
+  });
+});
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
